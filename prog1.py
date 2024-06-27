18c98a9b66942b43ea3d3a67da7fa996c8c891ec
Kristina <kristi_stepanova6969@mail.ru>
2024-06-28 02:08:43 +0300

    'pyQt_version_3'

--- prog1.py
+++ prog1.py
@@ -1,7 +1,7 @@
+from PyQt5 import Qt
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QApplication, QMainWindow
 from PyQt5.QtWidgets import QWidget, QTextEdit, QPushButton, QTableWidget,QTableWidgetItem
-
 import sys
 from prog2 import*
 
@@ -16,24 +16,73 @@ def btnLoadTable():
     row = 0
     for x in Gr.A:
 
-        win.tableWidget.setItem(row, 0, QTableWidgetItem(Gr.A[x].fam+' '+Gr.A[x].name+' '+Gr.A[x].surname))
-        win.tableWidget.setItem(row, 1, QTableWidgetItem(Gr.A[x].division))
-        win.tableWidget.setItem(row, 2, QTableWidgetItem(Gr.A[x].days))
-        win.tableWidget.setItem(row, 3, QTableWidgetItem(Gr.A[x].salary))
+        win.tableWidget.setItem(row, 0, QTableWidgetItem(Gr.A[x].fam))
+        win.tableWidget.setItem(row, 1, QTableWidgetItem(Gr.A[x].name))
+        win.tableWidget.setItem(row, 2, QTableWidgetItem(Gr.A[x].surname))
+        win.tableWidget.setItem(row, 3, QTableWidgetItem(Gr.A[x].division))
+        win.tableWidget.setItem(row, 4, QTableWidgetItem(Gr.A[x].days))
+        win.tableWidget.setItem(row, 5, QTableWidgetItem(Gr.A[x].salary))
         row += 1
     
 def btnAppendEmployee():
-    strEmployee = win.lineEdit.text()
+    List = [str(win.lineEdit_4.text()),str(win.lineEdit_5.text()),str(win.lineEdit_6.text()),\
+            str(win.lineEdit_7.text()),str(win.lineEdit_8.text()),str(win.lineEdit_9.text())]
 
-    Gr.appendEmployee(strEmployee)
-    win.lineEdit.clear()
+    Gr.appendEmployee(List)
+
     win.tableWidget.clear()
     btnLoadTable()
 
+def btnEditEmployee():
+    if win.lineEdit_2.text() == '' :
+        win.lineEdit_2.setText('1')
+
+    if win.lineEdit_3.text() == '':    
+        win.lineEdit_3.setText('1')
+    
+    x = int(win.lineEdit_2.text())-1
+    y = int(win.lineEdit_3.text())-1
+
+    if x <= win.tableWidget.rowCount() and y <= win.tableWidget.columnCount(): 
+         
+        List = [str(win.tableWidget.item(x,0).text()),\
+            str(win.tableWidget.item(x,1).text()),\
+            str(win.tableWidget.item(x,2).text()),\
+            str(win.tableWidget.item(x,3).text()),\
+            str(win.tableWidget.item(x,4).text()),\
+            str(win.tableWidget.item(x,5).text())]
+   
+        key = Gr.find_keyEmployee(List)
+         
+        if key != -1 :
+
+            win.tableWidget.setItem(x,y,QTableWidgetItem(str(win.lineEdit.text())))
+
+            List = [str(win.tableWidget.item(x,0).text()),\
+            str(win.tableWidget.item(x,1).text()),\
+            str(win.tableWidget.item(x,2).text()),\
+            str(win.tableWidget.item(x,3).text()),\
+            str(win.tableWidget.item(x,4).text()),\
+            str(win.tableWidget.item(x,5).text()),]
+             
+            print(List)     
+            Gr.editEmployee( key,List )
+
+def btnDelEmployee():
+    
+    List = [str(win.lineEdit_4.text()),str(win.lineEdit_5.text()),str(win.lineEdit_6.text()),\
+            str(win.lineEdit_7.text()),str(win.lineEdit_8.text()),str(win.lineEdit_9.text())]
+
+    Gr.delEmployee(List)
+  
+    win.tableWidget.clear()
+    
+    btnLoadTable()
     
 win.pushButton.clicked.connect(btnLoadTable)
-
 win.pushButton_3.clicked.connect(btnAppendEmployee)
+win.pushButton_4.clicked.connect(btnEditEmployee)
+win.pushButton_5.clicked.connect(btnDelEmployee)   
 
 win.show()
 sys.exit(app.exec())
--- prog2.py
+++ prog2.py
@@ -1,5 +1,5 @@
 class Employee:
-    def __init__(self, fam, name, surname, division, days, salary):
+    def __init__(self, fam='', name='', surname='', division='', days='', salary=''):
         self.fam = fam
         self.name = name
         self.surname = surname
@@ -9,14 +9,22 @@ class Employee:
     
     def getEmployee_forTable(self):
         w = []
-        print(self.fam+' '+self.name+' '+self.surname)
-        x = self.fam+' '+self.name+' '+self.surname
-        w.append(x)
+        w.append(self.fam)
+        w.append(self.name)
+        w.append(self.surname)
         w.append(self.division)
         w.append(self.days)
         w.append(self.salary)
-        print(w)
         return w
+    
+    def equval_Employee(self,B):
+    
+        return self.fam == B.fam and \
+               self.name == B.name and \
+               self.surname == B.surname and \
+               self.division == B.division and \
+               self.days == B.days and \
+               self.salary == B.salary
 
 class Grup:
     def __init__(self):
@@ -28,27 +36,51 @@ class Grup:
         for x in range(len(self.A)): 
             if x in self.A:
                 s += f'Employee {x+1}:\n'
-            s += str(self.A[x])
-            s += '\n'
+                s += str(self.A[x])
+                s += '\n'
         return s   
 
-    def appendEmployee(self, str_Employee):
-        parts = str_Employee.strip().split(" ")
-        self.A[self.count] = Employee(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5])
+    def appendEmployee(self, List):
+        new_Employee = Employee(*List)
+        self.A[self.count] = new_Employee
 
         self.count += 1
-        with open('text1.txt', 'a', encoding='utf-8') as file:
-            file.write('\n' + str_Employee)
+    
+    def editEmployee(self,x,List):
+        P = Employee(*List)
+        self.A[x] = P
 
+    def Str_Employee(self,line):
+        if line[-1] == '\n' : line = line[:-1] 
+        parts = line.strip().split("&")
+
+        return Employee(*parts)
+    
     def read_data(self, file_name):
         self.A = {}
         x = 0
         with open(file_name, 'r', encoding='utf-8') as file:
             for line in file:
-                if line[-1] == '\n': line = line[:-1]
-                parts = line.strip().split(" ")
-                
-                self.A[x] = Employee(parts[0], parts[1], parts[2], parts[3], parts[4], parts[5])
+                self.A[x] = self.Str_Employee(line)
 
                 x += 1
                 self.count += 1
+
+    def find_keyEmployee(self, List):
+
+        P = Employee(*List)
+        for x in self.A :
+            
+            if self.A[x].equval_Employee(P) :
+               return x
+
+        return -1    
+
+    def delEmployee(self, List):
+        P = Employee(*List)
+        for x in self.A :
+            if self.A[x].equval_Employee(P):
+                del self.A[x] 
+                self.count = self.count- 1
+    
+                break
